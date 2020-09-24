9a8d171e5283c356585fd289963ffbeb6d402c47
YulisaA <ynazurdia@correo.url.edu.gt>
2020-09-24 04:40:03 -0600

    Envío de data del frontend al backend.

--- src/app/components/dog-form/dog-form.component.ts
+++ src/app/components/dog-form/dog-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscriber } from 'rxjs';
+import { DogService } from '../../services/dog.service'
+
+interface HtmlInputEvent extends Event{
+  target: HTMLInputElement & EventTarget;
+}
 
 @Component({
   selector: 'app-dog-form',
@@ -7,9 +13,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DogFormComponent implements OnInit {
 
-  constructor() { }
+  file: File;
+  photoSelected: string | ArrayBuffer;
+
+  constructor(private dogService: DogService) { }
 
   ngOnInit() {
+  }
+
+  onPhotoSelected(event: HtmlInputEvent): void {
+    if(event.target.files && event.target.files[0]){
+      this.file = <File>event.target.files[0];
+      //Show image
+      const reader = new FileReader();
+      reader.onload = e => this.photoSelected = reader.result;
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  uploadInfo(name: HTMLInputElement, description: HTMLInputElement, years: HTMLInputElement, months: HTMLInputElement):boolean{
+      //Send data to service
+      this.dogService.createDog(name.value, description.value, years.value.toString(), months.value.toString(), this.file);
+      //.subscribe(res => console.log(res), err => console.log(err))
+      return false;
   }
 
 }
--- src/app/services/dog.service.ts
+++ src/app/services/dog.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DogService {
 
-  constructor() { }
+  serverURI = 'http://localhost:4000/dogs'
+
+  constructor(private http: HttpClient) { }
+
+  createDog(name: string, description: string, years: string, months: string, photo: File){
+    const formDog = new FormData();
+    formDog.append('name', name);
+    formDog.append('description', description);
+    formDog.append('years', years);
+    formDog.append('months', months);
+    formDog.append('photo', photo);
+    //return this.http.post(this.serverURI, formDog);
+  }
+
 }
+
